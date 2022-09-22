e3b593f5a2777ceea9e3e1079d366905ef9db575
Sulav Pokhrel <103086640+TrainYourFingers@users.noreply.github.com>
2022-09-22 15:42:54 +0545

    first commit

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:"jit",
+  purge:["../*.html"],
+  darkMode: "class",
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
